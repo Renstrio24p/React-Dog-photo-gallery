4227be5c5d2e956a8161362454283b8a12b8b7c0
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-05-22 12:17:37 +0800

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Card(props){
     return (
         <div className="card">
-            <img src={props.image} />
+            <img src={props.image} alt="dog photo" />
             <div>
                 <h1 className="type">{props.type}</h1>
                 <p className="definition">{props.description}</p>
